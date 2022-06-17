946426df5aeb014c23e1a5eb0693ac8783963727
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-06-17 17:13:41 -0300

    add some content to homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,31 @@
 import styles from '../styles/Home.module.scss';
+import SearchBar from '../components/SearchBar';
+import LinkBox from '../components/LinkBox';
+import ImgPokeball from '../assets/pokeball-icon.png';
 
-const Home = () => {  
+const Home = () => { 
+  
+  const search = (searchText: string) => {
+    console.log('You tried to search: ', searchText);
+  }
 
   return (
-    <div className={styles.teste}>Homezada</div>
+    <main className={styles.wrapper}>
+      <img src={ImgPokeball} alt="Transparent Pokeball" className={styles.transparentPokeball}/>
+
+      <h2>What are you looking for?</h2>
+
+      <SearchBar onEnter={search}/>
+
+      <section className={styles.linksContainer}>
+        <LinkBox mainText='Pokedex' color='79,193,166'/>
+        <LinkBox mainText='Moves' color='247,119,106'/>
+        <LinkBox mainText='Abilities' color='88,169,244'/>
+        <LinkBox mainText='Items' color='255,206,75'/>
+        <LinkBox mainText='Locations' color='124,82,140'/>
+        <LinkBox mainText='Type Charts' color='176,115,109'/>
+      </section>
+    </main>
   )
 }
 
