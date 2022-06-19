f335114287afa23fce0add5c27b766e29b3c8cd0
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-06-18 21:50:45 -0300

    transparent pokeball adjustment

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes as Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Pokedex from "./pages/Pokedex";
 
 const Router = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/pokedex' element={<Pokedex/>}></Route>
       </Switch>
     </BrowserRouter>
   )
