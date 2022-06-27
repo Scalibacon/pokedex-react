e0c8b11d64352dd616eab75c9622bbae7fa168b8
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-06-27 18:40:58 -0300

    some adjustments

--- src/components/LinkBox/index.tsx
+++ src/components/LinkBox/index.tsx
@@ -1,6 +1,7 @@
 import styles from './LinkBox.module.scss';
 import ImgPokeball from '../../assets/pokeball-icon.png';
 import { CSSProperties } from 'react';
+import { capitalizeFirstLetter } from '../../utils/stringUtils';
 
 interface LinkBoxProps {
   mainText: string,
@@ -12,11 +13,30 @@ interface LinkBoxProps {
 }
 
 const LinkBox = (props: LinkBoxProps) => {
+  const renderOptionalData = () => {
+    if(!props.image) return;
+
+    return (
+      <div className={styles.optionalContainer}>
+        { props.type1 ? <span>{capitalizeFirstLetter(props.type1)}</span> : ''}
+        { props.type2 ? <span>{capitalizeFirstLetter(props.type2)}</span> : ''}
+
+        <img className={styles.pokemonImage} src={props.image} alt={props.mainText}/>
+      </div>
+    )
+  }
+
   return (
-    <a href={props.url} className={styles.linkBox} style={{ "--background-color": props.color} as CSSProperties}>
+    <a 
+      href={props.url} 
+      className={`${styles.linkBox} ${props.type1 ? styles.pokemon : ''}`} 
+      style={{ "--background-color": props.color} as CSSProperties}
+    >
       <img src={ImgPokeball} alt="Transparent Pokeball"/>
 
-      <p className={styles.name}>{ props.mainText }</p>      
+      <p className={styles.name}>{ capitalizeFirstLetter(props.mainText) }</p>   
+
+      {renderOptionalData()}   
     </a>
   )
 }
--- src/pages/Pokedex.tsx
+++ src/pages/Pokedex.tsx
@@ -1,15 +1,78 @@
 import styles from '../styles/Pokedex.module.scss';
 import UpperButtons from '../components/UpperButtons';
+import LinkBox from '../components/LinkBox';
+import { useEffect, useState } from 'react';
+
+type PokemonResume = {
+  name: string,
+  id: number,
+  imageUrl: string,
+  type1: string,
+  type2?: string
+}
 
 const Pokedex = () => {
-  return(
-    <>
-      <UpperButtons title="Pokedex"/>
+  const [pokemonList, setPokemonList] = useState<PokemonResume[]>([]);
 
-      <div className={styles.pokedexWrapper}>                 
-      </div> 
-    </>
+  const fetchPokemons = async () => {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0');
+    const responseJson = await response.json();
+    const result = responseJson.results;
+
+    let pokemonsArray = [];
+    for(let i = 0; i < result.length; i++){
+      const pokemon = result[i];
+      const pokemonData = await fetchPokemonData(pokemon.name);
+
+      const pokemonResume = {
+        name: pokemonData.name,
+        id: pokemonData.id,
+        imageUrl: pokemonData.sprites.other['official-artwork'].front_default,
+        type1: pokemonData.types[0]?.type.name,
+        type2: pokemonData.types[1]?.type.name,
+      } as PokemonResume;
+
+      console.log(pokemonResume);
+
+      pokemonsArray.push(pokemonResume);
+    }
+
+    setPokemonList(pokemonsArray);
+  }
+
+  useEffect( () => {
+    fetchPokemons();
+  }, []);  
+
+  const fetchPokemonData = async (pokemonName: string) => {
+    const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    const pokemonData = await result.json();
+
+    return pokemonData;
+  }
+
+  return(
+    <div className={styles.pokedexWrapper}> 
+
+    <UpperButtons title="Pokedex"/>
     
+      <section className='cardsContainer'>
+        { pokemonList.map( pokemon => {
+          return (
+            <LinkBox 
+              key={pokemon.id} 
+              mainText={pokemon.name} 
+              color='79,193,166' 
+              image={pokemon.imageUrl}
+              type1={pokemon.type1}
+              type2={pokemon.type2}
+              url='/pokedex'                 
+            />
+          )
+        })}
+        
+      </section>               
+    </div> 
   )
 }
 
